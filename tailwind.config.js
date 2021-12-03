2353915d1645de6c0a26eea68487b2914263bc5b
thushakk4@gmail.com <thushakk4@gmail.com>
2021-12-02 22:10:32 -0430

    add mode jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // purge: [],
-  // mode: 'jit',
+  mode: 'jit',
   purge: ['./index.html', './src/**/*.vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
